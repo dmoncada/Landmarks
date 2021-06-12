b1573076233952f669f99d3884a6a28ee6e314d7
David Moncada <david_moncada@icloud.com>
2021-06-11 17:30:23 -0700

    Initial commit.

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by David Moncada on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
